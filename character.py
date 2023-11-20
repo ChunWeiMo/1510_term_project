d0ebc20de74fb7985331194cc384b2047264069d
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-20 00:51:09 -0800

    Add moving system

--- character.py
+++ character.py
@@ -2,10 +2,12 @@
 Character module.
 """
 import time
+import map
 
 
 def make_character():
-    character_stats = {"Level": 1, "HP": 100, "STR": 1, "DEF": 1, "CHR": 1, "SPD": 1, "LUK": 1}
+    character_stats = {"Level": 1, "HP": 100, "STR": 1,
+                       "DEF": 1, "CHR": 1, "SPD": 1, "LUK": 1}
     print("Welcome Hero! In this game there are 6 attributes:\n"
           "[1] HP (Health points) - If HP reaches 0, you die and the game is over.\n"
           "[2] STR (Strength) - Deal more damage to the enemy.\n"
@@ -16,7 +18,7 @@ def make_character():
     time.sleep(3)
     attribute_points = 10
     add_attribute_points(attribute_points, character_stats)
-    return character_stats
+    return {"Character_status": character_stats, "X-coordinate": 5, "Y-coordinate": 0}
 
 
 def add_attribute_points(attribute_points, character_stats):
@@ -36,3 +38,37 @@ def add_attribute_points(attribute_points, character_stats):
             character_stats["LUK"] += 1
         attribute_points -= 1
     print(f"Your stats are: {character_stats}\n")
+
+
+def validate_move(current_map, character, direction):
+    north_wall, west_all = 0, 0
+    south_wall, east_wall = map.walls(current_map)
+    can_move = True
+    if character["Y-coordinate"] <= north_wall and direction == 0:
+        can_move = False
+        print("You are stopped by North wall\n")
+    if character["X-coordinate"] >= east_wall and direction == 1:
+        can_move = False
+        print("You are stopped by East wall\n")
+    if character["Y-coordinate"] >= south_wall and direction == 2:
+        can_move = False
+        print("You are stopped by South wall\n")
+    if character["X-coordinate"] <= west_all and direction == 3:
+        can_move = False
+        print("You are stopped by West wall\n")
+    return can_move
+
+
+def move_character(character, direction):
+    if direction == 0:
+        character["Y-coordinate"] -= 1
+        print("moving toward North...")
+    if direction == 1:
+        character["X-coordinate"] += 1
+        print("moving toward East...")
+    if direction == 2:
+        character["Y-coordinate"] += 1
+        print("moving toward South...")
+    if direction == 3:
+        character["X-coordinate"] -= 1
+        print("moving toward West...")

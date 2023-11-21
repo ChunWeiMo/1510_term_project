5668fd92a2a56a8800e2bd8fb7feb80beda29c0b
Patricia Lo <pkwlo1992@gmail.com>
2023-11-21 01:22:23 -0800

    Add run_away()

--- battle.py
+++ battle.py
@@ -1,3 +1,17 @@
 """
 Battle module.
 """
+import random
+
+
+def run_away(character_dictionary):
+    chance = 50 + character_dictionary["Character_status"]["SPD"] + character_dictionary["Character_status"]["LUK"] * 2
+    random_number = random.randint(1, 100)
+    if chance >= random_number >= 1:
+        return True
+    else:
+        return False
+
+
+def fight(character_dictionary, enemy_appeared):
+    pass

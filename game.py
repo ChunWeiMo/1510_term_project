75af4eb21a648829283beac2a33526829f0e3192
Patricia Lo <pkwlo1992@gmail.com>
2023-11-21 01:22:05 -0800

    Add some enemy and battle functions to game()

--- game.py
+++ game.py
@@ -5,9 +5,9 @@ A________
 Patricia Lo
 A00959925
 """
-import time
 import character
 import map
+import enemy
 
 
 def game():
@@ -16,9 +16,7 @@ def game():
     """
     achieved_goal = False
     map_list = map.make_maps()
-    character_stats = character.make_character()  # dictionary of character Level and 6 stats
-    character_position = {"X-coordinate": 0, "Y-coordinate": 0}
-    time.sleep(2)
+    character_dictionary = character.make_character()  # dictionary of character Level and 6 stats
     print("In the small town of Moland there is a ruined dungeon where a dragon sleeps.\n"
           "It has been prophesized that he will wake up in 100 years to take revenge on the town that sealed him.\n"
           "The 100 years are almost up and the townsfolk are getting restless.\n"
@@ -26,19 +24,25 @@ def game():
           "Unfortunately it's your first time on the job. This is why you don't lie on your resume...\n"
           "You still have a few days before the prophesized time so you decide to go level up a bit in the "
           "nearby field.\n")
-    while character_stats["HP"] > 0 and not achieved_goal:
-        if character_stats["Level"] < 3:
-            current_map = map.select_map(character_stats, map_list)  # need to use char lvl and char LUK to select a map
-            map.describe_current_map(character_stats, current_map)
+    while character_dictionary["Character_status"]["HP"] > 0 and not achieved_goal:
+        if character_dictionary["Character_status"]["Level"] < 3:
+            current_map = map.select_map(character_dictionary, map_list)
+            map.describe_current_map(character_dictionary, current_map)
             # move around map
-            # event
-            # combat
+
+            # encounters?
+
+            # combat starts when you encounter an enemy
+            enemy_dictionary = enemy.enemy()
+            enemy_appeared = enemy.select_enemy(character_dictionary, enemy_dictionary)
+            user_input = enemy.ask_user(enemy_appeared)
+            enemy.battle_talk_escape(character_dictionary, user_input, enemy_appeared)
         else:
             print("A loud resounding roar can be heard from deep in the depths of the dungeon. Uh-oh...did the dragon "
                   "wake up while you were leveling up? You hurry towards the sound.")
             # next map you enter will be boss map
-            current_map = map.select_map(character_stats, map_list)
-    if character_stats["Current HP"] == 0:
+            current_map = map.select_map(character_dictionary, map_list)
+    if character_dictionary["Character_status"]["Current HP"] == 0:
         print("You died...The town of Moland, helpless against the dragon's resurrection, has gone up in flames, "
               "not a single human spared by the dragon's wrath.")
     if achieved_goal:

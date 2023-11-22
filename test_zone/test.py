e58e912a19d4671e9882eedbcede9edd9f6467da
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-22 05:27:58 -0800

    Test 4 cardinal vision

--- test_zone/test.py
+++ test_zone/test.py
@@ -1,8 +1,8 @@
 import character
 import map
 import random
-
-
+import vision
+    
 def main():
     map_list = map.make_maps()
     # map_element = map.select_map(character, map_list)
@@ -17,7 +17,7 @@ def main():
             coordinate = (column, row)
             current_map[coordinate] = "Empty"
 
-    map_elements = map_list[9]
+    map_elements = map_list[6]
     map.set_element_on_map(map_elements, "Door", current_map)
     map.set_element_on_map(map_elements, "Enemy", current_map)
     map.set_element_on_map(map_elements, "Chest", current_map)
@@ -25,25 +25,33 @@ def main():
     map.set_element_on_map(map_elements, "Boss", current_map)
     map.set_element_on_map(map_elements, "Final_boss", current_map)
     print(current_map)
+    # for coordinate in current_map:
+    #     print(coordinate)
     # south_wall, east_wall = map.walls(current_map)
 
     character_status = {'Level': 1, 'HP': 100, 'STR': 10,
                         'DEF': 1, 'CHR': 1, 'SPD': 1, 'LUK': 2}
     character_1 = {"character_status": character_status,
-                   "X-coordinate": 0, "Y-coordinate": 0}
+                   "X-coordinate": 4, "Y-coordinate": 6}
     
     character.start_from_door(map_elements, character_1)
+    print(character_1)
     
-    direction = 3
-    can_move = character.validate_move(current_map, character_1, direction)
-    print(can_move)
-    if can_move:
-        character.move_character(character_1, direction)
-        print(character_1)
-    if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
-        print(
-            f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
-
-
+    # direction = 3
+    # can_move = character.validate_move(current_map, character_1, direction)
+    # print(can_move)
+    # if can_move:
+    #     character.move_character(character_1, direction)
+    #     print(character_1)
+    # if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
+    #     print(
+    #         f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
+    vision_range = 3
+    vision.vision_north(character_1, current_map, vision_range)
+    vision.vision_east(character_1, current_map, vision_range)
+    vision.vision_south(character_1, current_map, vision_range)
+    vision.vision_west(character_1, current_map, vision_range)
+        
+    
 if __name__ == "__main__":
     main()

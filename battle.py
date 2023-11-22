0e7d695fa66acd07cdbc2e2ce45e3188c400ac9d
Patricia Lo <pkwlo1992@gmail.com>
2023-11-21 22:43:51 -0800

    Add some helper functions to battle.py

--- battle.py
+++ battle.py
@@ -16,81 +16,95 @@ def run_away(character_dictionary):
 
 def who_goes_first(character_dictionary, enemy_appeared):
     if character_dictionary["Character_status"]["SPD"] > enemy_appeared["SPD"]:
-        first_turn = "character"
+        turn = "character"
     elif character_dictionary["Character_status"]["SPD"] == enemy_appeared["SPD"]:
-        first_turn = random.randint(1, 2)
-        if first_turn == 1:
-            first_turn = "character"
+        turn = random.randint(1, 2)
+        if turn == 1:
+            turn = "character"
         else:
-            first_turn = "enemy"
+            turn = "enemy"
     else:
-        first_turn = "enemy"
-    return first_turn
+        turn = "enemy"
+    return turn
 
 
 def fight(character_dictionary, enemy_appeared, can_start):
     if can_start:
-        first_turn = who_goes_first(character_dictionary, enemy_appeared)
+        turn = who_goes_first(character_dictionary, enemy_appeared)
     else:
-        first_turn = "enemy"
+        turn = "enemy"
     while enemy_appeared["HP"] > 0 and character_dictionary["Character_status"]["HP"] > 0:
-        if first_turn == "character":
+        if turn == "character":
             if character_dictionary["Character_status"]["STR"] - enemy_appeared["DEF"] <= 0:
-                damage_to_enemy = 1
+                damage = 1
             else:
-                damage_to_enemy = character_dictionary["Character_status"]["STR"] - enemy_appeared["DEF"]
-            enemy_appeared["HP"] -= damage_to_enemy
+                damage = character_dictionary["Character_status"]["STR"] - enemy_appeared["DEF"]
+            enemy_appeared["HP"] -= damage
             if enemy_appeared["HP"] > 0:
-                print(f"You deal {damage_to_enemy} damage to the {enemy_appeared['Name']}!\n"
+                print(f"You deal {damage} damage to the {enemy_appeared['Name']}!\n"
                       f"The {enemy_appeared['Name']} has {enemy_appeared['HP']} HP left.")
             else:
-                print(f"You deal {damage_to_enemy} damage to the {enemy_appeared['Name']}!\n"
+                print(f"You deal {damage} damage to the {enemy_appeared['Name']}!\n"
                       f"The {enemy_appeared['Name']} has 0 HP left.")
-            if character_dictionary["Character_status"]["SPD"] >= enemy_appeared["SPD"] * 2:
-                enemy_appeared["HP"] -= damage_to_enemy
-                if enemy_appeared["HP"] > 0:
-                    print(f"You deal {damage_to_enemy} damage to the {enemy_appeared['Name']}!\n"
-                          f"The {enemy_appeared['Name']} has {enemy_appeared['HP']} HP left.")
-                else:
-                    print(f"You deal {damage_to_enemy} damage to the {enemy_appeared['Name']}!\n"
-                          f"The {enemy_appeared['Name']} has 0 HP left.")
-            first_turn = "enemy"
+            speedy(turn, character_dictionary, enemy_appeared, damage)
+            turn = "enemy"
         else:
             if enemy_appeared["STR"] - character_dictionary["Character_status"]["DEF"] <= 0:
-                damage_to_character = 1
+                damage = 1
             else:
-                damage_to_character = enemy_appeared["STR"] - character_dictionary["Character_status"]["DEF"]
-            character_dictionary["Character_status"]["HP"] -= damage_to_character
+                damage = enemy_appeared["STR"] - character_dictionary["Character_status"]["DEF"]
+            character_dictionary["Character_status"]["HP"] -= damage
             if character_dictionary['Character_status']['HP'] > 0:
-                print(f"The {enemy_appeared['Name']} dealt {damage_to_character} damage to you!\n"
+                print(f"The {enemy_appeared['Name']} dealt {damage} damage to you!\n"
                       f"You now have {character_dictionary['Character_status']['HP']} HP left.")
             else:
-                print(f"The {enemy_appeared['Name']} dealt {damage_to_character} damage to you!\n"
+                print(f"The {enemy_appeared['Name']} dealt {damage} damage to you!\n"
                       f"You now have 0 HP left.")
-            if enemy_appeared["SPD"] >= character_dictionary["Character_status"]["SPD"] * 2:
-                character_dictionary["Character_status"]["HP"] -= damage_to_character
-                if character_dictionary['Character_status']['HP'] > 0:
-                    print(f"The {enemy_appeared['Name']} dealt {damage_to_character} damage to you!\n"
-                          f"You now have {character_dictionary['Character_status']['HP']} HP left.")
-                else:
-                    print(f"The {enemy_appeared['Name']} dealt {damage_to_character} damage to you!\n"
-                          f"You now have 0 HP left.")
-            first_turn = "character"
+            speedy(turn, character_dictionary, enemy_appeared, damage)
+            turn = "character"
     if enemy_appeared["HP"] <= 0:
-        character_dictionary["EXP"] += enemy_appeared["EXP"]
-        print(f"{enemy_appeared['Name']} has been slain!\n"
-              f"You've gained {character_dictionary['Items']['Gold']} gold!\n"
-              f"You've gained {enemy_appeared['EXP']} experience!\n"
-              f"Your experience is now at {character_dictionary['EXP']}/100!")
+        enemy_defeated(character_dictionary, enemy_appeared)
         if character_dictionary["EXP"] >= 100:
-            character_dictionary["Character_status"]["Level"] += 1
-            print(f"You leveled up! You are now level {character_dictionary['Character_status']['Level']}")
-            attribute_points = 5
-            character_dictionary["EXP"] = 0
-            if character_dictionary["Character_status"]["Level"] == 2:
-                character_dictionary["Character_status"]["HP"] = 120
-            if character_dictionary["Character_status"]["Level"] == 3:
-                character_dictionary["Character_status"]["HP"] = 150
-            character.add_attribute_points(attribute_points, character_dictionary)
+            level_up(character_dictionary)
     if character_dictionary["Character_status"]["HP"] == 0:
         return
+
+
+def enemy_defeated(character_dictionary, enemy_appeared):
+    character_dictionary["EXP"] += enemy_appeared["EXP"]
+    character_dictionary['Items']['Gold'] += enemy_appeared["Gold"]
+    print(f"{enemy_appeared['Name']} has been slain!\n"
+          f"You've gained {enemy_appeared['Gold']} gold!\n"
+          f"You've gained {enemy_appeared['EXP']} experience!\n"
+          f"Your experience is now at {character_dictionary['EXP']}/100!")
+
+
+def level_up(character_dictionary):
+    character_dictionary["Character_status"]["Level"] += 1
+    print(f"You leveled up! You are now level {character_dictionary['Character_status']['Level']}")
+    attribute_points = 5
+    character_dictionary["EXP"] = 0
+    if character_dictionary["Character_status"]["Level"] == 2:
+        character_dictionary["Character_status"]["HP"] = 120
+    if character_dictionary["Character_status"]["Level"] == 3:
+        character_dictionary["Character_status"]["HP"] = 150
+    character.add_attribute_points(attribute_points, character_dictionary)
+
+
+def speedy(turn, character_dictionary, enemy_appeared, damage):
+    if turn == "character" and character_dictionary["Character_status"]["SPD"] >= enemy_appeared["SPD"] * 2:
+        enemy_appeared["HP"] -= damage
+        if enemy_appeared["HP"] > 0:
+            print(f"You deal {damage} damage to the {enemy_appeared['Name']}!\n"
+                  f"The {enemy_appeared['Name']} has {enemy_appeared['HP']} HP left.")
+        else:
+            print(f"You deal {damage} damage to the {enemy_appeared['Name']}!\n"
+                  f"The {enemy_appeared['Name']} has 0 HP left.")
+    if turn == "enemy" and enemy_appeared["SPD"] >= character_dictionary["Character_status"]["SPD"] * 2:
+        character_dictionary["Character_status"]["HP"] -= damage
+        if character_dictionary['Character_status']['HP'] > 0:
+            print(f"The {enemy_appeared['Name']} dealt {damage} damage to you!\n"
+                  f"You now have {character_dictionary['Character_status']['HP']} HP left.")
+        else:
+            print(f"The {enemy_appeared['Name']} dealt {damage} damage to you!\n"
+                  f"You now have 0 HP left.")

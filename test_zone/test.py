8f7c25c0198cdc72800c7e71b572d747da8223a8
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-24 02:35:36 -0800

    Add test moving

--- test_zone/test.py
+++ test_zone/test.py
@@ -2,7 +2,8 @@ import character
 import map
 import random
 import vision
-    
+
+
 def main():
     map_list = map.make_maps()
     # map_element = map.select_map(character, map_list)
@@ -32,27 +33,50 @@ def main():
     character_status = {'Level': 1, 'HP': 100, 'STR': 10,
                         'DEF': 1, 'CHR': 1, 'SPD': 1, 'LUK': 2}
     character_1 = {"character_status": character_status,
-                   "X-coordinate": 4, "Y-coordinate": 9}
-    
+                   "X-coordinate": 1, "Y-coordinate":5}
+
     # character.start_from_door(map_elements, character_1)
     # print(character_1)
     
-    # direction = 3
-    # can_move = character.validate_move(current_map, character_1, direction)
-    # print(can_move)
-    # if can_move:
-    #     character.move_character(character_1, direction)
-    #     print(character_1)
+    # ==================
+    # test standing
+    # ==================
     # if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
     #     print(
     #         f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
-    character.describe_current_location(current_map, character_1)
-    vision_range = 3
-    vision.vision_north(character_1, current_map, vision_range)
-    vision.vision_east(character_1, current_map, vision_range)
-    vision.vision_south(character_1, current_map, vision_range)
-    vision.vision_west(character_1, current_map, vision_range)
-        
+
+    # character.describe_current_location(current_map, character_1)
+    # vision_range = 3
+    # vision.print_vision(character_1, current_map, vision_range)
+    # print()
     
+    # ==================
+    # test text vision
+    # ==================
+    # vision.vision_north(character_1, current_map, vision_range)
+    # vision.vision_east(character_1, current_map, vision_range)
+    # vision.vision_south(character_1, current_map, vision_range)
+    # vision.vision_west(character_1, current_map, vision_range)
+
+    # ==================
+    # test moving
+    # ==================
+    direction = 0
+    vision_range = 3
+    while direction!= 5:
+        character.describe_current_location(current_map, character_1)
+        vision.print_vision(character_1, current_map, vision_range)
+        print()
+        direction = int(input("Enter a direction: "))
+        can_move = character.validate_move(current_map, character_1, direction)
+        if can_move:
+            character.move_character(character_1, direction)
+            # print(character_1)
+        if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
+            print(
+                f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
+
+        
+
 if __name__ == "__main__":
     main()

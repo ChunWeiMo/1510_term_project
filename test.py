70c1ea6874ab79fb388f1fe00a53e5ccdcae8499
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-29 00:21:19 -0800

    Test travel between maps

--- test.py
+++ test.py
@@ -2,58 +2,42 @@ import random
 from modules.exploration import map
 from modules.exploration import vision
 from modules.exploration import movement
+from modules.exploration import event
 
 def main():
-    map_list = map.make_maps()
-    # map_element = map.select_map(character, map_list)
-
-    # map.describe_current_map(character, map_element)
-
-    rows = 10
-    columns = 10
-    current_map = dict()
-    for row in range(rows):
-        for column in range(columns):
-            coordinate = (column, row)
-            current_map[coordinate] = "Empty"
-
-    map_elements = map_list[9]
-    map.set_element_on_map(map_elements, "Door", current_map)
-    map.set_element_on_map(map_elements, "Enemy", current_map)
-    map.set_element_on_map(map_elements, "Chest", current_map)
-    map.set_element_on_map(map_elements, "Healing_fountain", current_map)
-    map.set_element_on_map(map_elements, "Boss", current_map)
-    map.set_element_on_map(map_elements, "Final_boss", current_map)
-    # print(current_map)
-    # for coordinate in current_map:
-    #     print(coordinate)
-    # south_wall, east_wall = map.walls(current_map)
-
-    character_1 = {'Character_status': {'Level': 1, 'HP': 100, 'STR': 11, 'DEF': 1, 'CHR': 1, 'SPD': 1, 'LUK': 1},
-        'X-coordinate': 5, 'Y-coordinate': 5, 'EXP': 0, 'Items': {'Gold': 0, 'Potions': 0}, 'vision_range': 10}
     # character_1 = character.make_character()
     # print(character_1)
-
-    # character.start_from_door(map_elements, character_1)
-    # print(character_1)
+    character_1 = {'Character_status': {'Level': 1, 'HP': 100, 'STR': 11, 'DEF': 1, 'CHR': 1, 'SPD': 1, 'LUK': 1},
+                   'X-coordinate': 5, 'Y-coordinate': 5, 'EXP': 0, 'Items': {'Gold': 0, 'Potions': 0}, 'vision_range': 3}
     
+    map_list = map.map_list()
+    current_map = map.create_map(character_1, map_list)
+    movement.start_from_door(character_1, current_map)
+    # vision.print_vision(character_1, current_map)
+
     # ==================
     # test moving
     # ==================
     direction = 0
     while direction != 5:
-        movement.describe_current_location(current_map, character_1)
-        vision.print_vision(character_1, current_map, )
+        vision.print_vision(character_1, current_map)
         print()
-        direction = int(input("Enter a direction: "))
-        can_move = movement.validate_move(current_map, character_1, direction)
-        if can_move:
-            movement.move_character(character_1, direction)
-            # print(character_1)
-        if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
-            print(
-                f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
-
+        movement.describe_current_location(character_1, current_map)
+        try:
+            direction = int(input("Enter a direction: "))
+        except ValueError:
+            print(f"Invalid input.")
+        else:    
+            can_move = movement.validate_move(
+                character_1, direction, current_map)
+            if can_move:
+                movement.move_character(character_1, direction)
+                
+            if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
+                print(
+                    f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}')
+            if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] == "Door":
+                event.open_the_door(character_1)
 
 if __name__ == "__main__":
 

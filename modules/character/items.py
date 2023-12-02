8f516aa7b7e71a391e6f8c7d01bce53aa4317daf
Patricia Lo <pkwlo1992@gmail.com>
2023-12-01 23:04:51 -0800

    Remove catch_error_potion function

--- modules/character/items.py
+++ modules/character/items.py
@@ -18,7 +18,6 @@ def use_potion(character_dictionary):
 
     if character_dictionary["Items"]["Potions"] > 0:
         response = ask_player_potions(character_dictionary)
-        catch_potion_error(response, character_dictionary)
         heal_character(character_dictionary, response, max_hp)
     else:
         print("You have 0 potions to use.")
@@ -27,8 +26,8 @@ def use_potion(character_dictionary):
 
 def ask_player_potions(character_dictionary):
     response = int(input(f"You have {character_dictionary['Items']['Potions']} potions and "
-                     f"{character_dictionary['Character_status']['HP']} HP.\n"
-                     f"Enter how many potions you would like to use:\n"))
+                         f"{character_dictionary['Character_status']['HP']} HP.\n"
+                         f"Enter how many potions you would like to use:\n"))
     if response <= 0 or response > character_dictionary['Items']['Potions']:
         print(f"You must enter a number between 1 and {character_dictionary['Items']['Potions']}.")
         ask_player_potions(character_dictionary)
@@ -41,8 +40,3 @@ def heal_character(character_dictionary, response, max_hp):
         character_dictionary["Character_status"]["HP"] += response * 20
     else:
         character_dictionary["Character_status"]["HP"] = max_hp
-
-
-def catch_potion_error(response, character_dictionary):
-    if response <= 0:
-        raise IndexError(f"You must enter a number between 1 and {character_dictionary['Items']['Potions']}.")

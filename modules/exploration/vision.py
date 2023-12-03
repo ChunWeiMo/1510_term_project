6b7461509c50c180226b907235bab9a7d799110b
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-03 03:37:04 -0800

    Update key for new character dictionary

--- modules/exploration/vision.py
+++ modules/exploration/vision.py
@@ -80,9 +80,9 @@ def map_element_in_vision(character, current_map, column, row):
 def print_vision(character, current_map):
     map_icon = {'Empty': ' ', 'Door': 'D', 'Healing_fountain': 'H', 'Enemy': 'E', 'Boss': 'B', 'Final_boss': 'F',
                 'Chest': 'C', 'Merchant': 'M', 'Character': '#', 'EW_wall': '|', 'NS_wall': '-'}
-    for row in range(-character["vision_range"], character["vision_range"]+1):
+    for row in range(-character["Character_status"]["VIS"], character["Character_status"]["VIS"]+1):
         print()
-        for column in range(-character["vision_range"], character["vision_range"]+1):
+        for column in range(-character["Character_status"]["VIS"], character["Character_status"]["VIS"]+1):
             map_element = map_element_in_vision(
                 character, current_map, column, row)
 

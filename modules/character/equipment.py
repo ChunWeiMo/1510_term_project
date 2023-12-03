4c4987e153c19d898acd28fd254de159452a3938
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-03 02:51:43 -0800

    Add get_equipment and use_equipment

--- modules/character/equipment.py
+++ modules/character/equipment.py
@@ -1,35 +1,71 @@
 """
 equipment module
 """
+import random
+
 character_stats = {"Level": 1, "HP": 100, "STR": 1,
                    "DEF": 1, "CHR": 1, "SPD": 1, "LUK": 1, "VIS": 3}
-level1_equipment = {1: ["Steel sword", character_stats["STR"]+3],
-                    2: ["Small round shield", character_stats["DEF"]+3],
-                    3: ["Cloak", character_stats["SPD"]+3],
-                    4: ["Cheat coin", character_stats["LUK"]+3],
-                    5: ["Noble Insignia", character_stats["CHR"]+3],
-                    6: ["The lance of curses", character_stats["STR"]+6, character_stats["HP"]-30],
-                    7: ["Angel's winged garment", character_stats["SPD"]+6, character_stats["DEF"]-2],
-                    8: ["Heavy knight's armor", character_stats["DEF"]+6, character_stats["SPD"]-2],
-                    9: ["Beginner adventurer's guide", character_stats["STR"]+1, character_stats["DEF"]+1, character_stats["CHR"]+1, character_stats["SPD"]+1],
-                    10: ["Single-barrel telescope", character_stats["VIS"]+1],
-                    11: ["Giant hammer", character_stats["STR"]+5, character_stats["SPD"]-1]
-                        }
 
-level2_equipment = {1: ["Silver sword", character_stats["STR"]+7],
-                    2: ["Hero's spear", character_stats["STR"]+5, character_stats["SPD"]+2],
-                    3: ["Hermes shoes", character_stats["SPD"]+7],
-                    4: ["King's scepter", character_stats["STR"]+1, character_stats["CHR"]+6],
-                    5: ["Kiss of Rose Princess", character_stats["CHR"]+7],
-                    6: ["Platinum shield", character_stats["DEF"]+7],
-                    7: ["Oracle crystal ball", character_stats["VIS"]+2],
-                    8: ["Luxurious carriage", character_stats["SPD"]+3, character_stats["CHR"]+4],
-                    9: ["Spirits guardian", character_stats["DEF"]+3, character_stats["SPD"]+3],
-                    
-}
 
-level3_equipment = {1: ["Ragnarök", character_stats["STR"]+15],
-                    2: ["The Goddess's blessing", character_stats["DEF"]+15],
-                    3: ["Garuda's wing", character_stats["SPD"]+15],
-                    4: ["Dragon slayer", character_stats["STR"]+10, character_stats["SPD"]+5]
-                    }
+def level1_equipment_list():
+    return {1: ["Steel sword", ("STR", 3)],
+            2: ["Small round shield", ("DEF", 3)],
+            3: ["Cloak", ("SPD", 3)],
+            4: ["Cheat coin", ("LUK", 3)],
+            5: ["Noble insignia", ("CHR", 3)],
+            6: ["The lance of curses", ("STR", 6), ("HP", -30)],
+            7: ["Angel's winged garment", ("SPD", 6), ("DEF", -2)],
+            8: ["Heavy knight's armor", ("DEF", 6), ("SPD", -2)],
+            9: ["Beginner adventurer's guide", ("STR", 1), ("DEF", 1), ("CHR", 1), ("SPD", 1)],
+            10: ["Single-barrel telescope", ("VIS", 1)],
+            11: ["Giant hammer", ("STR", 5), ("SPD", -1)]
+            }
+
+
+def level2_equipment_list():
+    return {1: ["Silver sword", ("STR", 7)],
+            2: ["Hero's spear", ("STR", 5), ("SPD", 2)],
+            3: ["Hermes shoes", ("SPD", 7)],
+            4: ["King's scepter", ("STR", 1), ("CHR", 6)],
+            5: ["Kiss of Rose Princess", ("CHR", 7)],
+            6: ["Platinum shield", ("DEF", 7)],
+            7: ["Oracle crystal ball", ("VIS", 2)],
+            8: ["Luxurious carriage", ("SPD", 3), ("CHR", 4)],
+            9: ["Spirits guardian", ("DEF", 4),("SPD", 3)],
+            }
+
+
+def level3_equipment_list():
+    return {1: ["Ragnarök", character_stats["STR"]+15],
+            2: ["The Goddess's blessing", character_stats["DEF"]+15],
+            3: ["Garuda's wing", character_stats["SPD"]+15],
+            4: ["Dragon slayer", character_stats["STR"]+10, character_stats["SPD"]+5]
+            }
+
+
+def select_equipment(character_dictionary):
+    if character_dictionary['Character_status']['Level'] == 1:
+        equipments = level1_equipment_list()
+    elif character_dictionary['Character_status']['Level'] == 2:
+        equipments = level2_equipment_list()
+    else:
+        equipments = level3_equipment_list()
+        
+    while True:
+        equipment_number = random.randint(1, 99)
+        if equipment_number in equipments:
+            equipment = equipments[equipment_number]
+            break
+    print(f"You find {equipment[0]}!")
+    print(f"Ability of {equipment[0]}:")
+    for ability in equipment[1:]:
+        print(f"{ability[0]} {ability[1]}")
+    print()
+    return equipment, equipment_number
+    
+
+def use_equipment(character_dictionary, equipment, equipment_number):
+    character_dictionary["Equipment"] = equipment_number
+    for ability in equipment[1:]:
+        if ability[0] in character_dictionary['Character_status']:
+            character_dictionary['Character_status'][ability[0]] += ability[1]

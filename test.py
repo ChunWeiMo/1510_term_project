f4b474d6e40e026e6d46f9249b80d77c65b10637
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-04 11:14:26 -0800

    Integrate intro and make character

--- test.py
+++ test.py
@@ -4,29 +4,38 @@ from modules.exploration import map
 from modules.exploration import vision
 from modules.exploration import movement
 from modules.exploration import event
+from modules.exploration import story_lines
 from modules.battle import enemy
 from modules.menu import saveload
+from modules.character import character
 
 
 def main():
-    # character_1 = character.make_character()
-    # print(character_1)
-    character_stats = {"Level": 1, "HP": 80, "STR": 10,
-                       "DEF": 7, "CHR": 3, "SPD": 5, "LUK": 9, "VIS": 3}
-    character_name = "lionheartmo"
-    character_1 = {"Character_status": character_stats,
-                            "Name": character_name,
-                            "X-coordinate": 0,
-                            "Y-coordinate": 0,
-                            "EXP": 0,
-                            "Items": {"Gold": 0, "Potions": 0},
-                            "Equipment": ["The lance of curses", ("STR", 6), ("HP", -30)],
-                            "Debuff": {"Burn": 0}
-                            }
+    # character_dictionary = character.make_character()
+    # print(character_dictionary)
+    # character_stats = {"Level": 1, "HP": 80, "STR": 10,
+    #                    "DEF": 7, "CHR": 3, "SPD": 5, "LUK": 9, "VIS": 3}
+    # character_name = "lionheartmo"
+    # character_dictionary = {"Character_status": character_stats,
+    #                         "Name": character_name,
+    #                         "X-coordinate": 0,
+    #                         "Y-coordinate": 0,
+    #                         "EXP": 0,
+    #                         "Items": {"Gold": 0, "Potions": 0},
+    #                         "Equipment": ["The lance of curses", ("STR", 6), ("HP", -30)],
+    #                         "Debuff": {"Burn": 0}
+    #                         }
 
+    achieved_goal = False
+    achieved_goal_talk = False
+    print(story_lines.welcome)
+    character_dictionary = character.make_character()
+    main_story = story_lines.get_story(character_dictionary)
+    print(main_story["intro"])
+    
     map_list = map.maps()
-    current_map = map.create_map(character_1, map_list)
-    movement.start_from_door(character_1, current_map)
+    current_map = map.create_map(character_dictionary, map_list)
+    movement.start_from_door(character_dictionary, current_map)
 
     # ==================
     # test playing
@@ -34,8 +43,8 @@ def main():
     command = "not assign"
     while command.upper != "Q":
         print()
-        vision.print_vision(character_1, current_map)
-        movement.describe_current_location(character_1, current_map)
+        vision.print_vision(character_dictionary, current_map)
+        movement.describe_current_location(character_dictionary, current_map)
         
         print("Command list")
         print("Move a direction: N / E / S / W")
@@ -48,32 +57,32 @@ def main():
         if command.upper() == 'N' or command.upper() == 'E' or command.upper() == 'S' or command.upper() == 'W':
             direction = command.upper()
             can_move = movement.validate_move(
-                character_1, direction, current_map)
+                character_dictionary, direction, current_map)
             if can_move:
-                movement.move_character(character_1, direction)
-                if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] != "Empty":
+                movement.move_character(character_dictionary, direction)
+                if current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])] != "Empty":
                     print(
-                        f'You meet {current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])]}!')
+                        f'You meet {current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])]}!')
                     print()
-                if current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] == "Door":
-                    current_map = event.open_the_door(character_1, current_map)
-                elif current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] == "Enemy":
-                    event.encounter_an_enemy(character_1, current_map)
-                elif current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] == "Chest":
-                    event.find_a_chest(character_1, current_map)
-                elif current_map[(character_1["X-coordinate"], character_1["Y-coordinate"])] == "Healing_fountain":
-                    event.rest_at_fountain(character_1, current_map)
+                if current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])] == "Door":
+                    current_map = event.open_the_door(character_dictionary, current_map)
+                elif current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])] == "Enemy":
+                    event.encounter_an_enemy(character_dictionary, current_map)
+                elif current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])] == "Chest":
+                    event.find_a_chest(character_dictionary, current_map)
+                elif current_map[(character_dictionary["X-coordinate"], character_dictionary["Y-coordinate"])] == "Healing_fountain":
+                    event.rest_at_fountain(character_dictionary, current_map)
         elif command.upper() == "STATS":
-            print(character_1["Character_status"])
+            print(character_dictionary["Character_status"])
             print()
         elif command.upper() == 'Q':
             print("\nThank you for playing.")
             print("Close the game.")
             break
         elif command.upper() == 'SAVE':
-            saveload.savedata(character_1, current_map)
+            saveload.savedata(character_dictionary, current_map)
         elif command.upper() == "LOAD":
-            character_1, current_map = saveload.loaddata(character_1, current_map)
+            character_dictionary, current_map = saveload.loaddata(character_dictionary, current_map)
         else:
             print("\nInvalid command.")
             print("Please enter a command again.")

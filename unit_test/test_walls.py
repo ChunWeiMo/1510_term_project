fa94265ab5731b9bdf5bbae040c03c7d93301eee
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-06 20:05:35 -0800

    Change how to import modules

--- unit_test/test_walls.py
+++ unit_test/test_walls.py
@@ -1,4 +1,4 @@
-from modules.exploration import map
+from modules.exploration.map import walls
 from unittest import TestCase
 
 
@@ -7,11 +7,11 @@ class TestWalls(TestCase):
         expect_south_wall = 1
         expect_east_wall = 1
         current_map = {(0, 0): 'Empty', (1, 0): 'Empty', (1, 0): 'Empty', (1, 1): 'Empty'}
-        self.assertEqual((expect_south_wall, expect_east_wall), map.walls(current_map))
+        self.assertEqual((expect_south_wall, expect_east_wall), walls(current_map))
 
     def test_10_by_10_map(self):
         expect_south_wall = 9
         expect_east_wall = 9
         current_map = {(i, j): 'Empty' for i in range(10) for j in range(10)}
-        self.assertEqual((expect_south_wall, expect_east_wall), map.walls(current_map))
+        self.assertEqual((expect_south_wall, expect_east_wall), walls(current_map))
     

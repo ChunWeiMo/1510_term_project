163536f9b57a7cbf658e2adbec30945efeb74dff
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-07 02:33:07 -0800

    Delete text-base vision

--- modules/exploration/vision.py
+++ modules/exploration/vision.py
@@ -3,62 +3,6 @@ vision module
 """
 
 
-def vision_east(character, current_map, vision_range):
-    for vision_range in range(1, 4):
-        try:
-            map_element = current_map[(
-                character["X-coordinate"]+vision_range, character["Y-coordinate"])]
-        except KeyError:
-            print("There is a wall at your East.")
-            break
-        else:
-            if map_element != "Empty":
-                print(
-                    f"You can see {map_element} at East {vision_range} step.")
-
-
-def vision_west(character, current_map, vision_range):
-    for vision_range in range(1, 4):
-        try:
-            map_element = current_map[(
-                character["X-coordinate"]-vision_range, character["Y-coordinate"])]
-        except KeyError:
-            print("There is a wall at your West.")
-            break
-        else:
-            if map_element != "Empty":
-                print(
-                    f"You can see {map_element} at Wast {vision_range} step.")
-
-
-def vision_north(character, current_map, vision_range):
-    for vision_range in range(1, 4):
-        try:
-            map_element = current_map[(
-                character["X-coordinate"], character["Y-coordinate"]-vision_range)]
-        except KeyError:
-            print("There is a wall at your North.")
-            break
-        else:
-            if map_element != "Empty":
-                print(
-                    f"You can see {map_element} at North {vision_range} step.")
-
-
-def vision_south(character, current_map, vision_range):
-    for vision_range in range(1, 4):
-        try:
-            map_element = current_map[(
-                character["X-coordinate"], character["Y-coordinate"]+vision_range)]
-        except KeyError:
-            print("There is a wall at your South.")
-            break
-        else:
-            if map_element != "Empty":
-                print(
-                    f"You can see {map_element} at South {vision_range} step.")
-
-
 def map_element_in_vision(character, current_map, column, row):
     try:
         map_element = current_map[(

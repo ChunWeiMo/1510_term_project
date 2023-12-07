5b78140445203fb07cfa465b48b5687c7191c7eb
Patricia Lo <pkwlo1992@gmail.com>
2023-12-06 23:27:28 -0800

    Add unit tests for heal_character

--- unit_test/test_ask_player_potions.py
+++ unit_test/test_ask_player_potions.py
@@ -12,18 +12,18 @@ class TestAskPlayerPotions(TestCase):
 
     @patch('builtins.input', side_effect=['2'])
     def test_valid_input(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 2)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 2)
 
     @patch('builtins.input', side_effect=['0'])
     def test_use_zero_potions(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 0)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 0)
 
     @patch('builtins.input', side_effect=['not_a_number', '2'])
     def test_non_numeric_input_then_valid_input(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 2)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 2)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['not_a_number', '4'])
@@ -34,8 +34,8 @@ class TestAskPlayerPotions(TestCase):
 
     @patch('builtins.input', side_effect=['-1', '2'])
     def test_negative_input_then_valid_input(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 2)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 2)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['-7', '4'])
@@ -46,8 +46,8 @@ class TestAskPlayerPotions(TestCase):
 
     @patch('builtins.input', side_effect=['7', '4'])
     def test_input_greater_than_available_potions_then_valid_input(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 4)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 4)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['7', '4'])
@@ -58,8 +58,8 @@ class TestAskPlayerPotions(TestCase):
 
     @patch('builtins.input', side_effect=['a', '4'])
     def test_input_letter_then_valid_input(self, _):
-        result = ask_player_potions(self.character_dictionary)
-        self.assertEqual(result, 4)
+        expected = ask_player_potions(self.character_dictionary)
+        self.assertEqual(expected, 4)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['a', '4'])

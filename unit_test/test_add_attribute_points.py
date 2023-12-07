01b4e7317ed11189d85d467bc7d63ee5030df446
Patricia Lo <pkwlo1992@gmail.com>
2023-12-06 22:56:52 -0800

    Fix error in unit tests for character.py

--- unit_test/test_add_attribute_points.py
+++ unit_test/test_add_attribute_points.py
@@ -43,3 +43,22 @@ class TestAddPoints(TestCase):
                     "'LUK': 2, 'VIS': 3}\n\n")
         self.assertEqual(mock_output.getvalue(), expected)
 
+    @patch('builtins.input', side_effect=["a", 1, 2, 3, 10, 4, 5])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_incorrect_inputs(self, mock_output, _):
+        attribute_points = 5
+        character_dictionary = {"Character_status": {"Level": 1, "HP": 100, "STR": 3,
+                                                     "DEF": 2, "CHR": 3, "SPD": 3, "LUK": 1, "VIS": 3},
+                                "Name": "Chris",
+                                "X-coordinate": 0,
+                                "Y-coordinate": 0,
+                                "EXP": 0,
+                                "Items": {"Gold": 0, "Potions": 0},
+                                "Equipment": 0,
+                                "Debuff": {"Burn": 0}
+                                }
+        add_attribute_points(attribute_points, character_dictionary)
+        expected = ("\nPlease enter a number from 1-5.\n\nPlease enter a number from 1-5.\n\n"
+                    "Your stats are: {'Level': 1, 'HP': 100, 'STR': 4, 'DEF': 3, 'CHR': 4, 'SPD': 4, "
+                    "'LUK': 2, 'VIS': 3}\n\n")
+        self.assertEqual(mock_output.getvalue(), expected)

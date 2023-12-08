a5ecf48a579c7756785c6874ab70df6d248558ae
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-08 01:54:33 -0800

    Add docstring ad doctest in saveload.py

--- modules/menu/saveload.py
+++ modules/menu/saveload.py
@@ -1,8 +1,20 @@
 import json
 
 
-def savedata(character_dictionary, current_map):
-    with open("./character.json","w") as file_object:
+def savedata(character_dictionary: dict, current_map: dict):
+    """
+    Save playing data to json file.
+
+    :param character_dictionary: a dictionary of character attributes
+    :param current_map: a dictionary
+    :precondition: attribute_points must be a positive integer
+    :precondition: character_dictionary is a dictionary that includes character status, name, location, experience,
+    :precondition: current_map must have element-coordinate as key-value pair
+    :precondition: coordinate must be a tuple of integers
+    :postcondition: save character_dictionary to character.json
+    :postcondition: save current_map to current_map.json
+    """
+    with open("./character.json", "w") as file_object:
         json.dump(character_dictionary, file_object)
         converted_map = {str(key): value for key, value in current_map.items()}
     with open("./current_map.json", 'w') as file_object:
@@ -10,13 +22,34 @@ def savedata(character_dictionary, current_map):
         print(f"Playing data is saved successfully!")
         
 
-def ask_loaddata(character_dictionary, current_map):
-    is_load = input(("Unsaved data will be lost.\n Are you sure you want to load savedata? (Y/N)\n"))
+def ask_loaddata(character_dictionary: dict, current_map: dict) -> tuple:
+    """
+    Ask player whether to load playing data or not.
+
+    :param character_dictionary: a dictionary of character attributes
+    :param current_map: a dictionary
+    :precondition: attribute_points must be a positive integer
+    :precondition: character_dictionary is a dictionary that includes character status, name, location, experience,
+    :precondition: current_map must have element-coordinate as key-value pair
+    :precondition: coordinate must be a tuple of integers
+    :postcondition: input Y to load character_dictionary and current_map playing data
+    :return: a tuple
+    """
+    is_load = input("Unsaved data will be lost.\n Are you sure you want to load savedata? (Y/N)\n")
     if is_load.upper() == "Y":
         character_dictionary, current_map = loaddata()
     return character_dictionary, current_map
-    
-def loaddata():
+
+
+def loaddata() -> [tuple, None]:
+    """
+    Load playing data from json file.
+
+    :postcondition: load character.json and convert to character_dictionary
+    :postcondition: load current_map.json and convert to current_map
+    :return: a tuple if load playing data successfully
+    :return: None if load playing data failed
+    """
     print("Loading savedata...\n")
     try:
         with open("./character.json") as file_object:

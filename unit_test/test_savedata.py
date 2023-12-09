8cb83cdcbd3354a46c28200cd6703122528b6094
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-08 21:58:57 -0800

    Add unit test for savedata

---
+++ unit_test/test_savedata.py
@@ -0,0 +1,19 @@
+import os
+from unittest import TestCase
+from modules.menu.saveload import savedata
+
+
+class TestSaveDate(TestCase):
+    def setUp(self):
+        self.character_dictionary = {"name": "AAAA"}
+
+    def test_savedata_succefully(self):
+        current_map = {"(0, 0)": "Empty", "(1, 0)": "Empty", "(2, 0)": "Empty"}
+        savedata(self.character_dictionary, current_map)
+        self.assertTrue(os.path.exists("./character.json"))
+        self.assertTrue(os.path.exists("./current_map.json"))
+
+    def test_current_map_not_dict_raise_TypeError(self):
+        current_map = "current_map"
+        with self.assertRaises(TypeError):
+            savedata(self.character_dictionary, current_map)
